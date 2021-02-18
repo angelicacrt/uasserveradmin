8faff6f4461c5ae35cc9f14297028eac7545db1a
angelicacrt <angelicachristabelle66579@gmail.com>
2021-02-18 02:27:04 -0800

    next patching

---
+++ newcontent.php
@@ -0,0 +1,25 @@
+
+<?php
+// Create connection
+$ipnet = "172.26.0.2";
+$username = "root";
+$password = "bebasaja";
+$dbname = "Trucorp";
+
+$conn = new mysqli($ipnet, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+$sql = "SELECT COUNT(*) as total FROM users";
+$result = $conn->query($sql);
+
+if ($result->num_rows != 0) {
+$row = $result->fetch_assoc();
+ echo "Total user: " . $row["total"]. "<br>";
+} else {
+  echo "0 results";
+}
+$conn->close();
+
+?>
